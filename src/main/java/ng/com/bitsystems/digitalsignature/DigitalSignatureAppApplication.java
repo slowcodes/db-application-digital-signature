7481c091df512d4a5f814a854363eaf4708a9e09
Ezenna Kelechi <charlesezenna@gmail.com>
2020-08-04 23:32:22 +0100

    initial commit after setup

---
+++ src/main/java/ng/com/bitsystems/digitalsignature/DigitalSignatureAppApplication.java
@@ -0,0 +1,13 @@
+package ng.com.bitsystems.digitalsignature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignatureAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalSignatureAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ng/com/bitsystems/digitalsignature/DigitalSignatureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ng.com.bitsystems.digitalsignature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalSignatureAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
